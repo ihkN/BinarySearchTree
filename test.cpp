5104909bfdf362b8ca6ac5d3f8647b44635db4d9
cs0uth <saeidaliei2019@gmail.com>
2020-04-05 03:32:07 +0200

    add tests for find, begin methods

--- test.cpp
+++ test.cpp
@@ -50,51 +50,8 @@ void printLine(int num=30)
 
 int main()
 {
-   // Bst<int, std::string> tree;
-   // std::cout << tree;
-
-/*
- *    std::pair p1 = std::make_pair(12, "SR");
- *    std::pair p2 = std::make_pair(4, "QP");
- *    std::pair p3 = std::make_pair(8, "NE");
- *    tree.insert(std::make_pair(1, "AB"));
- *    tree.insert(std::make_pair(3, "GR"));
- *    tree.insert(std::make_pair(2, "DM"));
- *    tree.insert(std::make_pair(6, "RE"));
- *    tree.insert(p1);
- *    tree.insert(p2);
- *    std::cout << tree;
- *   
- *    auto f = tree.find(p2);
- *    std::cout << "f   = " << (*f).first << std::endl;
- *    f++;
- *    std::cout << "f++ = " << (*f).first << std::endl;
- *    ++f;
- *    std::cout << "++f = " << (*f).first << std::endl;
- *    f--;
- *    std::cout << "f-- = " << (*f).first << std::endl;
- *    --f;
- *    std::cout << "--f = " << (*f).first << std::endl;
- *   
- *    auto max = tree.findMax();
- *    std::cout << max.first << std::endl;
- *    auto min = tree.findMin();
- *    std::cout << min.first << ", " << min.second << std::endl;
- *    
- *    auto b = tree.begin();
- *    auto cb = tree.cbegin();
- *    std::cout << "begin: " << (*++b).first << std::endl;
- *    std::cout << "cbegin: " << (*++cb).first << std::endl;
- *
- *    std::cout << "******" << std::endl;
- *    std::cout << tree << std::endl;
- *
- *    tree.clear();
- *    std::cout << tree << std::endl;
- */
-
     Bst<int, double> tree;
-    auto vec = getRandPairs(5);
+    auto vec = getRandPairs(1000000);
 
     printLine();
     auto t1 = std::chrono::high_resolution_clock::now();
@@ -127,9 +84,21 @@ int main()
     auto p2 = vec.back();
     auto it2 = tree.find(p2);
     std::cout << (*it2) << std::endl;
+    std::cout << "max pair: " << tree.findMax() << std::endl;
+    std::cout << "min pair: " << tree.findMin() << std::endl;
 
-    Bst<int, double>::iterator it3 = tree.find(p2);
-    std::cout << (*it3) << std::endl;
+    printLine();
+    std::cout << "*test begin and end iterators:" << std::endl;
+    auto bg = tree.begin();
+    auto cbg = tree.cbegin();
+    std::cout << "begin: " << (*bg) << std::endl;
+    std::cout << "cbegin: " << (*cbg) << std::endl;
+    auto ed = tree.end();
+    auto ced = tree.cend();
+    --ed;
+    std::cout << "--end: " << (*ed) << std::endl;
+    std::cout << "vec.back: " << vec.back() << std::endl;
+
 
 
 }
